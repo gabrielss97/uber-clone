3ec560990c84f89a348afb2dcb010b583fd475a3
gabrielss97 <gabriel.ssalvador97@gmail.com>
2021-11-17 08:58:11 -0300

    configure store and navSlice

--- readme.md
+++ readme.md
@@ -1,8 +1,18 @@
-Building Uber Clone
+<h1>Building Uber Clone</h1>
 
-First Steps
+<h2>First Steps</h2>
 
 1- Set up redux ("yarn add @reduxjs/toolkit" and "yarn add react-redux")
    Now wrap the App with a PROVIDER and inside the tag provider we put store={} and create store.js in the main folder
+
+2- Now inside the store.js we put:
+<code>import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+})</code>
+
+and now create a reducer, in this case ->  nav:navReducer, then we create a folder named slices and put inside navSlice.js to store your initial states and reducers,
+in this js we will use <a href="https://redux-toolkit.js.org/api/createSlice">createSlice()</a> method
 
 Obs: In React Native flex is by default column
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -1,7 +1,32 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initiaState = {
-    origin:null,
-    destination:null,
-    travelTimeInformation:null
-}
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.Destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.TravelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const {setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions
+
+//Selectors
+export const selectOrigin = (state) => state.nav.origin
+export const selectDestination = (state) => state.nav.origin
+export const selectTravelTimeInformation = (state) => state.nav.origin
+
+export default navSlice.reducer
